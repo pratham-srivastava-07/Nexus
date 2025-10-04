878d43170de840881fba73016a564ded1ddcad72
Krish <prathamrajsri2003@gmail.com>
2025-10-04 08:52:06 +0530

    feat: ui

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="chats"
         options={{
-          title: 'Explore',
+          title: 'Chats',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
